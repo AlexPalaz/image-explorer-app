96ed1c18db0969dd3b495e4b63ac360d7e1ec6a9
Alessandro Palazzesi <alessandro.palazzesi@gmail.com>
2024-11-16 17:07:37 +0100

    feat: increase home title font size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function HomePage() {
   return (
     <div className="mt-8 px-8">
       <Section>
-        <div className="text-center mb-8 font-semibold">
+        <div className="text-center mb-8 font-semibold text-lg">
           Welcome to IEA!, an Image Explorer App where you can get images from
           Unsplash
         </div>
