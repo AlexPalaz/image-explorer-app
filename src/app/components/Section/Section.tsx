9f2201ab89a510a0e91fdb2541ce56adec386acc
Alessandro Palazzesi <alessandro.palazzesi@gmail.com>
2024-11-16 17:07:22 +0100

    feat: add max width on section container

--- src/app/components/Section/Section.tsx
+++ src/app/components/Section/Section.tsx
@@ -4,7 +4,7 @@ export type SectionProps = {
 
 export default function Section({ children }: SectionProps) {
   return (
-    <div className="section p-8 rounded-lg shadow-md bg-white">
+    <div className="section p-8 rounded-lg shadow-md bg-white max-w-[1920px] m-auto">
       {children}
     </div>
   );
