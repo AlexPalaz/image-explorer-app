786118b7b21ff0dbd03bf6e2f787599fff19cadc
Alessandro Palazzesi <alessandro.palazzesi@gmail.com>
2024-11-18 09:51:44 +0100

    feat: add set cookies next client

--- src/app/auth/signout/page.tsx
+++ src/app/auth/signout/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { setCookie } from "cookies-next";
 import { useEffect } from "react";
 
 export default function SignOut() {
   useEffect(() => {
+    setCookie("user", "", { secure: true });
     window.location.href = "/";
   }, []);
   return <></>;
